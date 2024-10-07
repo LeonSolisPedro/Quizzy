f2e0526890d009ef537721af160e1be6ed11f45a
Pedro León <pedro@wintercr.com>
2024-10-07 12:50:46 -0500

    Initial

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./views/index"
+import "./axios"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />
+  }
+]);
+
+//Bootstrap
+import "./customStyle.scss"
+
+
+//Sweetalert
+import "./sweetalert"
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ src/views/index.jsx
@@ -0,0 +1,10 @@
+import "./index.css"
+
+export default function Index() {
+
+  return (
+    <div>
+      <p>Hello there!</p>
+    </div>
+  )
+}
