58e5f76a252ef5ddaf58201a493944ea2211d074
Pedro León <pedro@wintercr.com>
2024-10-14 15:34:04 -0500

    Finishing with the settings view

--- src/views/Quizzes/setting.jsx
+++ src/views/Quizzes/setting.jsx
@@ -28,6 +28,19 @@ export default function Setting() {
   const [userTopic, setTopic] = useState(1);
 
 
+  //Allow multiple answers
+  const [allowMA, setAllowMA] = useState(false);
+
+
+  //Tags
+  const [selectedTags, setSelectedTags] = useState([{"id": 1,"name": "Tag1"},{"id": 2,"name": "Tag2"}]);
+  const [selectedTagLists, setSelectedTagLists] = useState([]);
+  const search2 = async (event) => {
+    await new Promise((r) => setTimeout(r, 1000));
+    setSelectedTagLists([{ "id": 1, "name": "Tag1" }, { "id": 2, "name": "Tag2" }, { "id": 3, "name": "Tag3" }, { "id": 4, "name": "Tag4" }]);
+  };
+
+
   //Searching and adding
   const [Check, setCheck] = useState(true);
   const [selectedUser, setSelectedUser] = useState('');
@@ -81,28 +94,28 @@ export default function Setting() {
 
   return (
     <form>
-      <div class="mb-3">
-        <label class="form-label">Title</label>
-        <input type="email" class="form-control" value={"Quizz 1"} />
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input type="email" className="form-control" value={"Quizz 1"} />
       </div>
-      <div class="mb-3">
-        <label class="form-label">Description</label>
+      <div className="mb-3">
+        <label className="form-label">Description</label>
         <Tiptap ref={tiptapRef} content={"<p>Hello there</p>"} />
       </div>
-      <div class="mb-3">
-        <label class="form-label">Image</label>
+      <div className="mb-3">
+        <label className="form-label">Image</label>
         <div className="div-image-pedro-settings">
-          <input type="file" onChange={onImageChange} class="form-control" />
+          <input type="file" onChange={onImageChange} className="form-control" />
           <img className="image-pedro-settings rounded" alt="preview image" src={image} />
         </div>
       </div>
       <div className="mb-3">
-        <label class="form-label">Topic {userTopic}</label>
+        <label className="form-label">Topic</label>
         <div className="topic-settings-pedro">
           {topics.map(topic => (
-            <div key={topic.id} class="form-check">
-              <input checked={userTopic === topic.id} onChange={() => setTopic(topic.id)} class="form-check-input" type="radio" />
-              <label class="form-check-label">
+            <div key={topic.id} className="form-check">
+              <input checked={userTopic === topic.id} onChange={() => setTopic(topic.id)} className="form-check-input" type="radio" />
+              <label className="form-check-label">
                 {topic.name}
               </label>
             </div>
@@ -110,19 +123,49 @@ export default function Setting() {
         </div>
       </div>
 
-      <div class="mb-3">
-        <label class="form-label mb-2">Access Status</label>
+      <div className="mb-3">
+        <label className="form-label">Allow multiple answers</label>
+        <div className="topic-settings-pedro">
+          <div className="form-check">
+            <input checked={allowMA == true} onChange={() => setAllowMA(true)} className="form-check-input" type="radio" />
+            <label className="form-check-label">
+              Yes
+            </label>
+          </div>
+          <div className="form-check">
+            <input checked={allowMA == false} onChange={() => setAllowMA(false)} className="form-check-input" type="radio" />
+            <label className="form-check-label">
+              No
+            </label>
+          </div>
+        </div>
+      </div>
 
-        <div class="form-check form-switch">
-          <input class="form-check-input" type="checkbox" checked={Check ? true : false} role="switch" onChange={(e) => setCheck(e.target.checked)} />
-          <label class="form-check-label">Make private</label>
+      <div className="mb-3">
+        <label className="form-label">Tags</label>
+        <AutoComplete
+          field="name"
+          value={selectedTags}
+          suggestions={selectedTagLists}
+          completeMethod={search2}
+          onChange={(e) => setSelectedTags(e.value)}
+          multiple
+          forceSelection />
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label mb-3 d-flex flex-wrap align-items-center">Access status: {Check ? <span className="ms-2 badge rounded-pill text-bg-light">Private</span> : <span className="ms-2 badge text-bg-success">Public</span>}</label>
+
+        <div className="form-check form-switch">
+          <input className="form-check-input" type="checkbox" checked={Check ? true : false} role="switch" onChange={(e) => setCheck(e.target.checked)} />
+          <label className="form-check-label">Make private</label>
         </div>
       </div>
 
       <div className="mb-3" style={{ display: Check ? 'block' : 'none' }}>
-        <label class="form-label">Add User</label>
+        <label className="form-label">Add User</label>
         {/* Render above based on Check true and false */}
-        <div class="d-flex">
+        <div className="d-flex">
           <AutoComplete
             field="name"
             value={selectedUser}
@@ -133,9 +176,9 @@ export default function Setting() {
             onClear={e => setCanAddUser(false)}
             onKeyUp={e => setCanAddUser(false)}
             forceSelection />
-          <button type="button" onClick={e => addUserToTable()} disabled={!canAddUser} class="btn btn-primary btn-sm"><FontAwesomeIcon icon={faPlus} /></button>
+          <button type="button" onClick={e => addUserToTable()} disabled={!canAddUser} className="btn btn-primary btn-sm"><FontAwesomeIcon icon={faPlus} /></button>
         </div>
-        <div class="form-text">Only the users below have access to the quizz, search by name or email and added it to the table</div>
+        <div className="form-text">Only the users below have access to the quizz, search by name or email and added it to the table</div>
         <Table data={userList} sort={sort} theme={theme} layout={{ custom: true, horizontalScroll: true }}>
           {(tableList) => (
             <>
@@ -152,7 +195,7 @@ export default function Setting() {
                     <Cell>{item.name}</Cell>
                     <Cell>{item.email}</Cell>
                     <Cell>
-                      <button type="button" class="btn btn-light btn-sm" onClick={() => handleRemove(item.id)}>Remove</button>
+                      <button type="button" className="btn btn-light btn-sm" onClick={() => handleRemove(item.id)}>Remove</button>
                     </Cell>
                   </Row>
                 ))}
@@ -164,7 +207,7 @@ export default function Setting() {
       </div>
 
 
-      <button type="submit" class="btn btn-primary">Save</button>
+      <button type="submit" className="btn btn-primary">Save</button>
     </form>
   )
 }
