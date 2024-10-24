fc15709b7d0c2d39ab3ab7b68aa924c86a372768
Pedro León <pedro@wintercr.com>
2024-10-24 11:04:57 -0500

    Adding close menu

--- src/views/Index/index.jsx
+++ src/views/Index/index.jsx
@@ -4,7 +4,7 @@ import LoginSignup from "../../components/loginsignup";
 import { toggleNavBar } from "../../store/user"
 import { useSelector, useDispatch } from 'react-redux'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faFileSignature, faHouse, faSquareCheck, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faFileSignature, faHouse, faSquareCheck, faUser, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Index() {
@@ -21,38 +21,39 @@ export default function Index() {
         <div className="row">
           <div className="col-lg-2 px-sm-2 px-0 bg-awesome-nav">
             <aside id="awesome-nav" className={showNavBar ? 'active-nav' : ''}>
-            <div className="d-flex flex-column align-items-start px-3 pt-2 text-white min-vh-100">
-              <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start">
-                <li className="nav-item">
-                  <Link to={`/`} className="nav-link align-middle px-0" onClick={() => dispatch(toggleNavBar())}>
-                    <FontAwesomeIcon icon={faHouse} /> <span className="ms-1">Welcome</span>
-                  </Link>
-                </li>
-                {isLogged && (
-                  <>
-                    <li>
-                      <Link to={`/myquizzes`} className="nav-link px-0 align-middle" onClick={() => dispatch(toggleNavBar())}>
-                        <FontAwesomeIcon icon={faFileSignature} /> <span className="ms-1">Quizzes</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to={`/myanswers`} className="nav-link px-0 align-middle" onClick={() => dispatch(toggleNavBar())}>
-                      <FontAwesomeIcon icon={faCheck} /> <span className="ms-1">My answers</span>
-                      </Link>
-                    </li>
-                  </>
-                )}
-                {isAdmin && (
-                  <li>
-                    <Link to={`/admin`} className="nav-link px-0 align-middle" onClick={() => dispatch(toggleNavBar())}>
-                     <FontAwesomeIcon icon={faUser} /> <span className="ms-1">Admin</span>
+             <FontAwesomeIcon className="closenav d-lg-none" style={{color: "#D6D6D6"}} icon={faXmark} onClick={() => dispatch(toggleNavBar())} />
+              <div className="d-flex flex-column align-items-start px-3 pt-2 text-white min-vh-100">
+                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start">
+                  <li className="nav-item">
+                    <Link to={`/`} className="nav-link align-middle px-0" onClick={() => dispatch(toggleNavBar())}>
+                      <FontAwesomeIcon icon={faHouse} /> <span className="ms-1">Welcome</span>
                     </Link>
                   </li>
-                )}
-              </ul>
-            </div>
+                  {isLogged && (
+                    <>
+                      <li>
+                        <Link to={`/myquizzes`} className="nav-link px-0 align-middle" onClick={() => dispatch(toggleNavBar())}>
+                          <FontAwesomeIcon icon={faFileSignature} /> <span className="ms-1">Quizzes</span>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to={`/myanswers`} className="nav-link px-0 align-middle" onClick={() => dispatch(toggleNavBar())}>
+                          <FontAwesomeIcon icon={faCheck} /> <span className="ms-1">My answers</span>
+                        </Link>
+                      </li>
+                    </>
+                  )}
+                  {isAdmin && (
+                    <li>
+                      <Link to={`/admin`} className="nav-link px-0 align-middle" onClick={() => dispatch(toggleNavBar())}>
+                        <FontAwesomeIcon icon={faUser} /> <span className="ms-1">Admin</span>
+                      </Link>
+                    </li>
+                  )}
+                </ul>
+              </div>
             </aside>
-            
+
           </div>
           <div className="col-lg-10 p-3 p-sm-5">
             <Outlet />
