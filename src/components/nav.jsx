97ffe8a7c6c4a5c820f6b2def40eb9dbc21c559b
Pedro León <pedro@wintercr.com>
2024-10-23 20:45:08 -0500

    Adding the cute hamburger icon

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,6 +3,8 @@ import image from "../assets/quizzy.png"
 import { useDispatch } from 'react-redux'
 import { toggleNavBar } from "../store/user"
 import "./nav.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Nav() {
@@ -11,10 +13,16 @@ export default function Nav() {
   return (
     <nav className="navbar">
       <div className="container-fluid">
-        <a onClick={() => dispatch(toggleNavBar())} class="navbar-brand" href="#">
-          <img src={image} alt="Logo" width="45" height="45" class="d-inline-block align-text-top logo-navbar-pedrito" />
-          Quizzy
-        </a>
+        <div className="d-flex align-items-center">
+          <div className="cutehamburger" onClick={() => dispatch(toggleNavBar())}>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+          <Link to={`/`} class="navbar-brand d-flex align-items-center gap-2">
+            <img src={image} alt="Logo" width="45" height="45" class="d-inline-block align-text-top logo-navbar-pedrito" />
+            Quizzy
+          </Link>
+        </div>
+
         <p data-bs-toggle="modal" data-bs-target="#userLoginModal">Hello there</p>
       </div>
     </nav>
